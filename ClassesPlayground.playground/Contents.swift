d9f888de10e2fa7a9bd7cd88574033ffeb3fd200
talm8994 <tom.tom.pop13@gmail.com>
2015-12-07 13:53:52 -0700

    kghk,ckdtuyrs

--- ClassesPlayground.playground/Contents.swift
+++ ClassesPlayground.playground/Contents.swift
@@ -2,4 +2,31 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var str = "use classes"
+
+class PracticeClass
+{
+    var favoriteFood :String
+    var numberOfSkateboards :Int
+    var hasBrokenBone :Bool
+    
+    init()
+    {
+        favoriteFood = String()
+        numberOfSkateboards = Int()
+        hasBrokenBone = Bool()
+    }
+    
+    convenience init(hasBrokenBone :Bool)
+    {
+        self.init()
+        self.hasBrokenBone = hasBrokenBone
+    }
+}
+var sampleInstance = PracticeClass()
+print(sampleInstance.favoriteFood)
+print(sampleInstance.hasBrokenBone)
+sampleInstance.hasBrokenBone = true
+print(sampleInstance.hasBrokenBone)
+var otherInstance = PracticeClass(true)
+print(otherInstance.hasBrokenBone)
