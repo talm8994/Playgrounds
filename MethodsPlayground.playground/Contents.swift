6db14a8dd6cc55e63cc80bc002c8f51da8aac33a
talm8994 <tom.tom.pop13@gmail.com>
2015-12-07 12:34:16 -0700

    new stuff and things

--- MethodsPlayground.playground/Contents.swift
+++ MethodsPlayground.playground/Contents.swift
@@ -16,6 +16,45 @@ func isTired() -> Bool
 
 print(isTired())
 
+func doesSomething() ->String
+{
+    return "does something stupid"
+}
+
+print(doesSomething())
+
+func checkValue(someNumber :Int) -> String
+{
+    var answer :String = ""
+    
+    if(someNumber < -5)
+    {
+        answer = "that number is smoll"
+    }
+    else if (someNumber == 0)
+    {
+        answer = "the first even number🌚"
+    }
+    else
+    {
+        answer = "that nuber is not smoll"
+    }
+    
+    return answer
+}
+
+print(checkValue(453, words:"akdf;ja"))
+func multiParameterMthod(words :String, a :Int, v : Double) -> String
+{
+    var combinedString = words + " \(a)  \(v) "
+    
+    return combinedString
+}
+print(multiParameterMthod("Thomas", a:, v: 987.75))
+
+print(multiParameterMthod("Words for class", a: 6, v: 30.1))
+
+
 // sup dog
 
 
