6ac894a876f22d278f0a62b5de7b6b16e133199e
talm8994 <tom.tom.pop13@gmail.com>
2015-12-07 11:58:41 -0700

    methods

---
+++ MethodsPlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var title = "Methods in Swift"
+// adam how is you today 
+func doesNothing()
+{
+    print("boring")
+}
+//how about you trevor
+func isTired() -> Bool
+{
+    return false
+}
+
+print(isTired())
+
+// sup dog
+
+
+
